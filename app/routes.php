e9eb92b823f92788c65b79e62b15b0b34fa525a3
Rafaell Lycan <sonny.webdsg@gmail.com>
2015-01-04 19:53:02 -0200

    Add otherwise route if not exists

--- app/routes.php
+++ app/routes.php
@@ -12,14 +12,15 @@
 */
 
 // Default
-Route::get('/', array('as' => 'home', 'uses' => 'HomeController@showWelcome'));
+Route::get('/', array('as' => 'home', 'uses' => 'HomeController@index'));
 
 // Tasks - API
-Route::get('/tasks', array('as' => 'get_tasks', 'uses' => 'TasksController@index'));
-Route::post('/tasks', array('as' => 'new_task', 'uses' => 'TasksController@store'));
-Route::get('/tasks/{id}', array('as' => 'get_task', 'uses' => 'TasksController@show'));
-Route::put('/tasks/{id}', array('as' => 'update_task', 'uses' => 'TasksController@update'));
-Route::delete('/tasks/{id}', array('as' => 'delete_task', 'uses' => 'TasksController@delete'));
+Route::get('api', array('as' => 'api', 'uses' => 'HomeController@api'));
+Route::get('api/tasks', array('as' => 'get_tasks', 'uses' => 'TasksController@index'));
+Route::post('api/tasks', array('as' => 'new_task', 'uses' => 'TasksController@store'));
+Route::get('api/tasks/{id}', array('as' => 'get_task', 'uses' => 'TasksController@show'));
+Route::put('api/tasks/{id}', array('as' => 'update_task', 'uses' => 'TasksController@update'));
+Route::delete('api/tasks/{id}', array('as' => 'delete_task', 'uses' => 'TasksController@delete'));
 
 // Account - User
 Route::get('/account', array('as' => 'account', 'uses' => 'AccountController@index'));
@@ -28,3 +29,12 @@ Route::get('/account/logout', array('as' => 'account_logout', 'uses' => 'Account
 Route::get('/account/register', array('as' => 'new_account', 'uses' => 'AccountController@login'));
 Route::get('/account/profile', array('as' => 'get_account', 'uses' => 'AccountController@profile'));
 Route::get('/account/change-password', array('as' => 'change_password', 'uses' => 'AccountController@password'));
+
+App::missing(function($exception)
+{
+    $err_message = array(
+      'error_code' => 'This route does\'t exists'
+    );
+
+    return Response::json($err_message, 400);
+});
