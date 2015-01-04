8ddfbcd62c6d4057cf8b78edadad2cc71e0a8619
Rafaell Lycan <sonny.webdsg@gmail.com>
2015-01-04 19:55:05 -0200

    add others views

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,33 @@
+<?php
+
+class HomeController extends BaseController {
+
+	/*
+	|--------------------------------------------------------------------------
+	| Default Home Controller
+	|--------------------------------------------------------------------------
+	|
+	| You may wish to use controllers instead of, or in addition to, Closure
+	| based routes. That's great! Here is an example controller method to
+	| get you started. To route to this controller, just add the route:
+	|
+	|	Route::get('/', 'HomeController@showWelcome');
+	|
+	*/
+
+	public function index()
+	{
+		return View::make('hello');
+	}
+
+	public function api()
+	{
+		$api_message = array(
+			'message' => 'Todo List API'
+		);
+
+		return Response::json($api_message, 200);
+
+	}
+
+}
