d82dc995c384adbf02a720a2b68b7dc672ce141c
Rafaell Lycan <sonny.webdsg@gmail.com>
2015-01-05 13:48:17 -0200

    Add debugbar

--- app/config/app.php
+++ app/config/app.php
@@ -122,7 +122,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Way\Generators\GeneratorsServiceProvider',
-
+		'Barryvdh\Debugbar\ServiceProvider',
 	),
 
 	/*
@@ -189,7 +189,6 @@ return array(
 		'URL'               => 'Illuminate\Support\Facades\URL',
 		'Validator'         => 'Illuminate\Support\Facades\Validator',
 		'View'              => 'Illuminate\Support\Facades\View',
-
 	),
 
 );
